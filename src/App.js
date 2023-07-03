c32320da00ae24260f76fcf1e3cd3cf823dc08cb
yaswanth <yaswanth9802@gmail.com>
2023-07-03 20:35:03 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Timer from "./components/Countdowntimer/Timer.js";
 
 function App(){
   return(
-    <BrowserRouter basename="/">
+    <BrowserRouter>
       <Routes>
         <Route path='/' element={<Layout/>} className="routing">
           <Route index element={<Clock/>}></Route>
